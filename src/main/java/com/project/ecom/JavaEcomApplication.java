d77761bfc751d88354c64f8ac0758ad64930d430
Anurag Kumar <pumbatimon473@gmail.com>
2025-06-29 05:20:21 +0530

    Enabled Jpa Auditing

--- src/main/java/com/project/ecom/JavaEcomApplication.java
+++ src/main/java/com/project/ecom/JavaEcomApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @EnableCaching
 @SpringBootApplication
 @EnableConfigurationProperties({
--- src/main/java/com/project/ecom/models/BaseModel.java
+++ src/main/java/com/project/ecom/models/BaseModel.java
@@ -1,13 +1,16 @@
 package com.project.ecom.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @MappedSuperclass
@@ -15,4 +18,11 @@ public abstract class BaseModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
