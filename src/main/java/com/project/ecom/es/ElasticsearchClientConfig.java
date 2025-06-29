a521d3e26578a150465d96232cb5ef8824c3da72
Anurag Kumar <pumbatimon473@gmail.com>
2025-06-29 12:01:13 +0530

    ElasticsearchClientConfig: Sourcing the value of ES host and port from the system environment variable

--- src/main/java/com/project/ecom/es/ElasticsearchClientConfig.java
+++ src/main/java/com/project/ecom/es/ElasticsearchClientConfig.java
@@ -1,5 +1,6 @@
 package com.project.ecom.es;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
@@ -9,11 +10,14 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.project.ecom.es.repository")
 public class ElasticsearchClientConfig extends ElasticsearchConfiguration {
+    @Value("${ECOM_ES_HOST_AND_PORT}")
+    private String ES_HOST_AND_PORT;
+
     @Bean
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo("localhost:9201")
+                .connectedTo(ES_HOST_AND_PORT)
                 .build();
     }
 }
